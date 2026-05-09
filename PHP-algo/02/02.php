37491c90eab04f7b94599085a411d826c6e0e4a1
Franck-dev-hub <franck.spadotto@yahoo.com>
2026-05-09 22:27:44 +0200

    update(exo 02): with built-in function

--- PHP-algo/02/02.php
+++ PHP-algo/02/02.php
@@ -1,14 +1,6 @@
 <?php
-$char = (string)"*";
 $text_input = (string)readline("Enter a text : ");
-$text_output =(string)"";
 
-for ($i = 0; $i < (strlen($text_input)); $i++) {
-    $text_output .= $text_input[$i];
-
-    if ($i < strlen($text_input) - 1) {
-        $text_output .= "*";
-    }
-}
+$text_output = implode('*', str_split($text_input));
 
 echo $text_output . PHP_EOL;
