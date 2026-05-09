8b467a8649041420950f84e98d811e65d0a23c9b
Franck-dev-hub <franck.spadotto@yahoo.com>
2026-05-09 22:48:25 +0200

    update(exo 03): use builtin function

--- PHP-algo/03/03.php
+++ PHP-algo/03/03.php
@@ -1,9 +1,4 @@
 <?php
 $text_input =(string)readline("Enter a text : ");
-$text_output = "";
 
-for ($i = strlen($text_input) - 1; $i >= 0; $i--) {
-    $text_output .= $text_input[$i];
-}
-
-echo $text_output . PHP_EOL;
+echo strrev($text_input) . PHP_EOL;
